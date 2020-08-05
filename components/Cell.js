4f07bdbb4e4a1042b84f59f6fc8ababaed891279
Henrique Dias Campos <henrique.campos@mv.com.br>
2020-08-04 23:43:21 -0300

    teste animate

--- components/Cell.js
+++ components/Cell.js
@@ -1,23 +1,70 @@
 import React, {useRef, useState} from 'react';
 import styles from '../style';
-import {Animated, View, Text, PanResponder} from 'react-native';
+import {
+  Animated,
+  View,
+  Text,
+  PanResponder,
+  Button,
+  Pressable,
+} from 'react-native';
 
 export default function Cell({cell_, handleUpdateCell}) {
+  // fadeAnim will be used as the value for opacity. Initial Value: 0
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+  const [cell, setCell] = useState(cell_);
+
+  const fadeIn = () => {
+    console.log('passei');
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 1000,
+    }).start();
+  };
+
+  return (
+    <Pressable
+      style={[styles.cellContainer, {translateX: fadeAnim}]}
+      onPress={fadeIn}>
+      {/* <Animated.View
+        style={[
+          styles.cell,
+          {
+            opacity: fadeAnim,
+          },
+        ]}
+        onPress={fadeIn}>
+        <Text>{cell.cell}</Text>
+      </Animated.View> */}
+    </Pressable>
+  );
+}
+
+function CellComPanRespoder({cell_, handleUpdateCell}) {
   const [cell, setCell] = useState(cell_);
 
   const pan = useRef(new Animated.ValueXY()).current;
 
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
-    onPanResponderMove: Animated.event([
-      null,
-      {
-        dx: pan.x,
-        dy: pan.y,
-      },
-    ]),
+    onPanResponderMove: true
+      ? Animated.event([
+          null,
+          {
+            dx: pan.x,
+          },
+        ])
+      : Animated.event([
+          null,
+          {
+            dy: pan.y,
+          },
+        ]),
     onPanResponderGrant: () => {
-      pan.setOffset({x: pan.x._value, y: pan.y._value});
+      pan.setOffset({
+        x: pan.x._value,
+        y: pan.y._value,
+      });
     },
     onPanResponderEnd: () => {
       pan.flattenOffset();
